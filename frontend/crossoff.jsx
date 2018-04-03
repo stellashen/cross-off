30488660c075aaeb948fd3985dd6dc440f119fb8
Yangchen Shen <shyangch@gmail.com>
2018-04-03 16:52:13 -0700

    Add reducers and store

--- frontend/crossoff.jsx
+++ frontend/crossoff.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  // testing in console: window.getState() to see the state shape
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome</h1>, root);
 });
