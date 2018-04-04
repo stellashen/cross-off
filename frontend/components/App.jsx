5d82792e2d1884c409e930b64eb43bb98848decc
Yangchen Shen <shyangch@gmail.com>
2018-04-03 17:43:14 -0700

    Add app and root components, update entry file

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Cross Off</h1>
+  </div>
+);
+
+export default App;
--- frontend/crossoff.jsx
+++ frontend/crossoff.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
@@ -9,5 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
