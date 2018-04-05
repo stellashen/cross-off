82f739dd14705e4d9cf8fa894453a248e04a9acf
Yangchen Shen <shyangch@gmail.com>
2018-04-04 23:48:26 -0700

    Edit splash CSS

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,27 +5,63 @@ export default () => {
   // const logo = require('./logo_transparent_background.png');
   return (
   <div className="splash">
-    <section class="splash-left">
+    <section className="first-page">
+      <section class="splash-left">
 
-      <div>
-      <img src="assets/crossoff.png" /><h2>your tasks, </h2>
-      </div>
-      <h2 id="manage">manage your life.</h2>
+        <div>
+        <img src="assets/crossoff.png" /><h2>your tasks, </h2>
+        </div>
+        <h2 id="manage">manage your life.</h2>
 
-      <p id="first-line"> With CrossOff, you can create todos, jot down ideas,</p>
-      <div id="second-line">
-        <p> and celebrate your success by crossing your tasks off!</p>
-        <img src="assets/7281-person-raising-both-hands-in-celebration.png"/>
-      </div>
+        <p id="first-line"> With CrossOff, you can create todos, jot down ideas,</p>
+        <div id="second-line">
+          <p> and celebrate your success by crossing your tasks off!</p>
+          <img src="assets/7281-person-raising-both-hands-in-celebration.png"/>
+        </div>
 
-      <button type="button" className="signup button mainbutton">Create Account</button>
+        <button type="button" className="signup button mainbutton">Create Account</button>
 
+      </section>
+
+      <section class="splash-right">
+        <div id="laptop">
+          <img id="checklist" src="assets/checklist" />
+        </div>
+      </section>
     </section>
 
-    <section class="splash-right">
-      <div id="laptop">
-        <img id="checklist" src="assets/checklist" />
-      </div>
+    <section className="second-page">
+      <h2>More Content TBD</h2>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
+      <p>enaopirhgnepawoi;egnvpowaienbv;oqENV;qoenbvq;owBENqowevnqwbnveo;qbnvqo;wINVWo;enq;bn</p>
     </section>
   </div>
 );
