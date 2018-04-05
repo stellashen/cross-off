2e1512beb893d589aad80c06708dbebe1b94ae23
Yangchen Shen <shyangch@gmail.com>
2018-04-05 09:56:28 -0700

    Import stylesheets to application.scss in order, and add preloadedState
    If there is a current user, pass this preloadedState to configureStore,
    and then remove currentUser from the window.

--- frontend/crossoff.jsx
+++ frontend/crossoff.jsx
@@ -4,7 +4,14 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   // testing in console: window.getState() to see the state shape
   window.getState = store.getState;
   window.dispatch = store.dispatch;
