a4038af2b411f856d29f27396e142c0cf8c9c2be
Yangchen Shen <shyangch@gmail.com>
2018-04-06 01:36:29 -0700

    make the 1st page of homepage match to the browser window height

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -8,7 +8,7 @@ import SignUpFormContainer from './session_form/signup_form_container';
 import SignInFormContainer from './session_form/signin_form_container';
 import Sidebar from './sidebar/sidebar';
 const App = () => (
-  <div>
+  <div id="app">
     <AuthRoute exact path="/" component={NavBarContainer} />
     <Route exact path="/" component={Splash} />
     <Route exact path="/" component={Footer} />
