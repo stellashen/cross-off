feb81d097cd6ecc9dae8ffb5f37a8652a08e625e
Yangchen Shen <shyangch@gmail.com>
2018-04-06 16:40:03 -0700

    Restructure user_profile.jsx: display username when exists, else display email

--- frontend/components/sidebar/user_profile/user_profile.jsx
+++ frontend/components/sidebar/user_profile/user_profile.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-export default ({ currentUser, logout }) => {
-  return (
-    <div className="user-profile">
-      <p className="email">{currentUser.email}</p>
-      <button className="signout button" onClick={logout}>Log Out</button>
-    </div>
-  );
-};
+class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  displayUserInfo() {
+    const currentUser = this.props.currentUser;
+    if (currentUser.username.length !== 0) {
+      return currentUser.username;
+    } else {
+      return currentUser.email;
+    }
+  }
+  render() {
+    return (
+      <div className="user-profile">
+        <p className="username">{this.displayUserInfo()}</p>
+        <button
+          className="signout button"
+          onClick={this.props.logout}>Log Out</button>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
