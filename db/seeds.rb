658fd08082f076854be3f77d114736bfe61c72ac
Yangchen Shen <shyangch@gmail.com>
2018-04-05 20:23:20 -0700

    Seed db with demo_user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+demo_user = User.create!(
+  username: 'guest',
+  email: 'guest@crossoff.com',
+  password: 'password'
+)
