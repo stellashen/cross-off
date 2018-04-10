4b1994070417519ce84c825f139dc0075968e9ee
Yangchen Shen <shyangch@gmail.com>
2018-04-10 14:14:19 -0700

    When delete a list, delete the tasks within that list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ApplicationRecord
   validates :name, :user_id, presence: true
   validates :name, uniqueness: true
   belongs_to :user
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
