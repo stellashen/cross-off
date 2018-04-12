dbb1460b4e95e5fd04983dcdb5ac7c2c45596b13
Yangchen Shen <shyangch@gmail.com>
2018-04-12 13:32:19 -0700

    Try fetchList for task_index to fix bug - didn't work

--- frontend/components/tasks/task_index_container.jsx
+++ frontend/components/tasks/task_index_container.jsx
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import React from 'react';
 import TaskIndex from './task_index';
 import { withRouter } from 'react-router-dom';
+import { fetchList } from '../../actions/list_actions';
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -10,6 +11,12 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchList: (id) => dispatch(fetchList(id)),
+  };
+};
+
 export default withRouter(connect(
-  mapStateToProps, null
+  mapStateToProps, mapDispatchToProps
 )(TaskIndex));
