c78d6c22d983ae7666c7c81daa2074e512de71a1
Yangchen Shen <shyangch@gmail.com>
2018-05-12 03:43:53 -0700

    Move currentTask piece of state to under ui

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
 import listsReducer from './lists_reducer';
 import tasksReducer from './tasks_reducer';
-import currentTaskReducer from './current_task_reducer';
 import tagsReducer from './tags_reducer';
 import addTagsReducer from './add_tags_reducer';
 
 export default combineReducers({
   lists: listsReducer,
   tasks: tasksReducer,
-  currentTask: currentTaskReducer,
   tags: tagsReducer,
   taggings: addTagsReducer,
 });
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import modal from './modal_reducer';
+import currentTaskReducer from './current_task_reducer';
 
 export default combineReducers({
-  modal
+  modal,
+  currentTask: currentTaskReducer,
 });
