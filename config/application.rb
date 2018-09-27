babbe9f69327e653e55082c3e442ff7533c80519
Yangchen Shen <shyangch@gmail.com>
2018-09-26 23:33:21 -0700

    stop Rails from connecting to db during compilation
    Otherwise, there will be error when Heroku is compiling assets.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module CrossOff
     config.load_defaults 5.1
 
     config.assets.paths << Rails.root.join('app', 'assets', 'images', 'logo')
+
+    config.assets.initialize_on_precompile = false
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
