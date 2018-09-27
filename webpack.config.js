a60b0ad07052623e67257a0877772db44cf32cd1
Yangchen Shen <shyangch@gmail.com>
2018-09-27 15:06:30 -0700

    delete public assets folder
    no precompile needed any more & production mode fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+var path = require("path");
 var webpack = require("webpack");
 
 var plugins = []; // if using any plugins for both dev and production
@@ -6,8 +6,8 @@ var devPlugins = []; // if using any plugins for development
 
 var prodPlugins = [
   new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
+    "process.env": {
+      NODE_ENV: JSON.stringify("production")
     }
   }),
   new webpack.optimize.UglifyJsPlugin({
@@ -18,31 +18,31 @@ var prodPlugins = [
 ];
 
 plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+  process.env.NODE_ENV === "production" ? prodPlugins : devPlugins
 );
 
 module.exports = {
-	context: __dirname,
-	entry: './frontend/crossoff.jsx',
-	output: {
-		path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-		filename: 'bundle.js'
-	},
-	plugins: plugins,
-	module: {
-		loaders: [
-			{
-				test: [/\.jsx?$/, /\.js?$/],
-				exclude: /(node_modules)/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['env', 'react']
-				}
-			},
-		]
-	},
-	devtool: 'source-map',
-	resolve: {
-		extensions: ['.js', '.jsx', '*']
-	}
+  context: __dirname,
+  entry: "./frontend/crossoff.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env", "react"]
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
